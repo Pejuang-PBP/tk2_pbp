1ffa63c1d4addc2bab54d62c4af858f984181276
Adrian Ardizza <adrian.ardizza@gmail.com>
2021-12-20 21:29:32 +0700

    Update CookieRequest to support multiple cookies

--- lib/helpers/authenticated_request.dart
+++ lib/helpers/authenticated_request.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart' show kIsWeb;
 
 class CookieRequest {
   Map<String, String> headers = {};
+  Map<String, String> cookies = {};
   final http.Client _client = http.Client();
   bool loggedIn = false;
 
@@ -16,7 +17,7 @@ class CookieRequest {
     http.Response response =
         await _client.post(Uri.parse(url), body: data, headers: headers);
 
-    updateCookie(response);
+    _updateCookie(response);
 
     if (response.statusCode == 200) {
       loggedIn = true;
@@ -31,7 +32,7 @@ class CookieRequest {
     }
     http.Response response =
         await _client.get(Uri.parse(url), headers: headers);
-    updateCookie(response);
+    _updateCookie(response);
     return json.decode(response.body); // Expects and returns JSON request body
   }
 
@@ -42,16 +43,52 @@ class CookieRequest {
     }
     http.Response response =
         await _client.post(Uri.parse(url), body: data, headers: headers);
-    updateCookie(response);
+    _updateCookie(response);
     return json.decode(response.body); // Expects and returns JSON request body
   }
 
-  void updateCookie(http.Response response) {
-    String? rawCookie = response.headers['set-cookie'];
-    if (rawCookie != null) {
-      int index = rawCookie.indexOf(';');
-      headers['cookie'] =
-          (index == -1) ? rawCookie : rawCookie.substring(0, index);
+  void _updateCookie(http.Response response) {
+    String? allSetCookie = response.headers['set-cookie'];
+
+    if (allSetCookie != null) {
+      var setCookies = allSetCookie.split(',');
+
+      for (var setCookie in setCookies) {
+        var cookies = setCookie.split(';');
+
+        for (var cookie in cookies) {
+          _setCookie(cookie);
+        }
+      }
+
+      headers['cookie'] = _generateCookieHeader();
     }
+  }
+
+  void _setCookie(String rawCookie) {
+    if (rawCookie.isNotEmpty) {
+      var keyValue = rawCookie.split('=');
+      if (keyValue.length == 2) {
+        var key = keyValue[0].trim();
+        var value = keyValue[1];
+
+        // ignore keys that aren't cookies
+        if (key == 'path' || key == 'expires') return;
+
+        cookies[key] = value;
+      }
+    }
+  }
+
+  String _generateCookieHeader() {
+    String cookie = "";
+
+    for (var key in cookies.keys) {
+      if (cookie.isNotEmpty) cookie += ";";
+      String? newCookie = cookies[key];
+      cookie += '$key=$newCookie';
+    }
+
+    return cookie;
   }
 }
