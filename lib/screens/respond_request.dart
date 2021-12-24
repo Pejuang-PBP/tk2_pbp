35c1e5081b60d9b4b501086f76f17823cc86f067
Romi Fadhurrohman Nabil <romi.fadhurrohman@gmail.com>
2021-12-24 23:59:23 +0700

    Adding respond req donor dart file

---
+++ lib/screens/respond_request.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+import 'package:tk2_pbp/helpers/authenticated_request.dart';
+import 'package:tk2_pbp/components/menu_items.dart';
+import 'package:tk2_pbp/components/page_header.dart';
+
+class RespondDonorPage extends StatefulWidget {
+  const RespondDonorPage({Key? key}) : super(key: key);
+  @override
+  _RespondDonorState createState() => _RespondDonorState();
+}
+
+class _RespondDonorState extends State<RespondDonorPage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      final request = Provider.of<CookieRequest>(context, listen: false);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Respond Request Donor'),
+          backgroundColor: const Color.fromRGBO(0, 41, 84, 1),
+          actions: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.add_alert),
+              tooltip: 'Notification',
+              onPressed: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('This is notifications')));
+              },
+            ),
+          ],
+        ),
+        body: Center(
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            // Column is also a layout widget. It takes a list of children and
+            // arranges them vertically. By default, it sizes itself to fit its
+            // children horizontally, and tries to be as tall as its parent.
+            //
+            // Invoke "debug painting" (press "p" in the console, choose the
+            // "Toggle Debug Paint" action from the Flutter Inspector in Android
+            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+            // to see the wireframe for each widget.
+            //
+            // Column has various properties to control how it sizes itself and
+            // how it positions its children. Here we use mainAxisAlignment to
+            // center the children vertically; the main axis here is the vertical
+            // axis because Columns are vertical (the cross axis would be
+            // horizontal).
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              const PageHeader(
+                  title: "Request Donor",
+                  subtitle: "Select one of the actions below."),
+              MenuItem(
+                  icon: const Icon(Icons.bloodtype_outlined, size: 32.0),
+                  title: "Create Donation Request",
+                  subtitle:
+                      "You have not created a Donation Request, click here to create one.",
+                  onClick: () {}),
+            ],
+          ),
+        ));
+  }
+}
